cc137dbd36808b889f6d0d8ee68f38291305bed2
donmemedo <donmemedo@gmail.com>
2023-10-28 11:53:57 +0330

    Project Whisper Phase Train.

--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ import os
 
 os.system(
     'python3 ./transformers/examples/pytorch/speech_recognition/run_speech_recognition_seq2seq.py '
-    '--model_name_or_path="../models/whisper_medium" --dataset_path="../datasets/Common_Voice_Corpus_15" '
+    '--model_name_or_path="openai/whisper-medium" --dataset_path="../datasets/Common_Voice_Corpus_15" '
     '--dataset_config_name="fa" --language="persian" --train_split_name="train+validation" --eval_split_name="test" '
     '--max_steps="5000" --output_dir="./whisper-medium-fa" --per_device_train_batch_size="16" '
     '--gradient_accumulation_steps="2" --per_device_eval_batch_size="16" --logging_steps="25" --learning_rate="1e-5" '
@@ -12,14 +12,12 @@ os.system(
     '--save_steps="100" --generation_max_length="225" --preprocessing_num_workers="16" '
     '--length_column_name="input_length" --max_duration_in_seconds="30" --text_column_name="sentence" '
     '--freeze_feature_encoder="False" --gradient_checkpointing --group_by_length --overwrite_output_dir '
-    '--do_train --do_eval --predict_with_generate --audio_column_name path'
+    '--do_train --do_eval --predict_with_generate --audio_column_name="path"'
 )
-print("yaya")
 # subprocess.run(['cd //home//makhataei//Projects//STT//src//transformers//examples//pytorch//speech_recognition','ls'])
 #
 # subprocess.run('python run_speech_recognition_seq2seq.py --model_name_or_path="openai/whisper-small" --dataset_name="mozilla-foundation/common_voice_11_0" --dataset_config_name="hi" --language="hindi" 	--train_split_name="train+validation" --eval_split_name="test" --max_steps="5000" --output_dir="./whisper-small-hi" --per_device_train_batch_size="16" --gradient_accumulation_steps="2" --per_device_eval_batch_size="16" --logging_steps="25" --learning_rate="1e-5" --warmup_steps="500" --evaluation_strategy="steps" --eval_steps="1000" --save_strategy="steps" --save_steps="1000" --generation_max_length="225" --preprocessing_num_workers="16" --length_column_name="input_length" --max_duration_in_seconds="30" --text_column_name="sentence" --freeze_feature_encoder="False" --gradient_checkpointing --group_by_length --fp16 --overwrite_output_dir --do_train --do_eval --predict_with_generate --use_auth_token')
 # trainer
-print("yaya")
 # python run_speech_recognition_seq2seq.py \
 # 	--model_name_or_path="openai/whisper-small" \
 # 	--dataset_name="mozilla-foundation/common_voice_11_0" \
