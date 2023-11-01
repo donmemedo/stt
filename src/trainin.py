2257c02b7413fb7eb96ddcb91e09f1b31cefd573
donmemedo <donmemedo@gmail.com>
2023-11-01 10:25:41 +0330

    Whisper Trainer for Mozilla Persian Corpus.

--- src/trainin.py
+++ src/trainin.py
@@ -1,126 +1,102 @@
 from datasets import Dataset
 import pandas as pd
-from datasets import Audio
-import gc
 import librosa
-import numpy
+from transformers import (
+    WhisperFeatureExtractor,
+    WhisperTokenizer,
+    WhisperProcessor,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+    WhisperForConditionalGeneration,
+)
+import torch
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+import evaluate
+
 
 ## we will load the both of the data here.
-
 train_df = pd.read_csv("../datasets/Common_Voice_Corpus_15/fa/train.csv")
 test_df = pd.read_csv("../datasets/Common_Voice_Corpus_15/fa/test.csv")
-PATH = '/home/makhataei/Projects/STT/datasets/Common_Voice_Corpus_15/fa/clips/'
-## we will rename the columns as “audio”, “sentence”.
-# train_df.remove_columns(["accents", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
-# test_df.remove_columns(["accents", "age", "client_id", "down_votes", "gender", "locale", "path", "segment", "up_votes"])
-# train_df.columns = ["gender", "sentence"]
-# test_df.columns = ["gender", "sentence"]
+PATH = "/home/makhataei/Projects/STT/datasets/Common_Voice_Corpus_15/fa/clips/"
+
 audio1 = []
 for i in list(train_df.path):
-    audio1.append(librosa.load(path=PATH+i,sr=16000)[0])
-train_df['audio'] = audio1
+    audio1.append(librosa.load(path=PATH + i, sr=16000)[0])
+train_df["audio"] = audio1
 
 audio2 = []
 for i in list(test_df.path):
-    audio2.append(librosa.load(path=PATH+i,sr=16000)[0])
-test_df['audio'] = audio2
+    audio2.append(librosa.load(path=PATH + i, sr=16000)[0])
+test_df["audio"] = audio2
 
 train_dataset = Dataset.from_pandas(train_df)
 test_dataset = Dataset.from_pandas(test_df)
-
-
-
-# train_dataset = train_dataset.cast_column("audio", Audio(sampling_rate=16000))
-# test_dataset = test_dataset.cast_column("audio", Audio(sampling_rate=16000))
-
-## import feature extractor
-
-from transformers import WhisperFeatureExtractor
-
 feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
 
-
 ## Load WhisperTokenizer
-
-from transformers import WhisperTokenizer
-
-tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-small", language="Persian", task="transcribe")
+tokenizer = WhisperTokenizer.from_pretrained(
+    "openai/whisper-small", language="Persian", task="transcribe"
+)
 
 ## Combine To Create A WhisperProcessor
+processor = WhisperProcessor.from_pretrained(
+    "openai/whisper-small", language="Persian", task="transcribe"
+)
 
-from transformers import WhisperProcessor
-
-processor = WhisperProcessor.from_pretrained("openai/whisper-small", language="Persian", task="transcribe")
 
 def prepare_dataset(examples):
-
     # compute log-Mel input features from input audio array
-
     audio = examples["audio"]
-
-    examples["input_features"] = feature_extractor( audio , sampling_rate=16000).input_features[0]
-
+    examples["input_features"] = feature_extractor(
+        audio, sampling_rate=16000
+    ).input_features[0]
     # del examples["audio"]
-
     sentences = examples["sentence"]
-
     # encode target text to label ids
-
     examples["labels"] = tokenizer(sentences).input_ids
-
     # del examples["sentence"]
-
     return examples
-print("yaboooo")
+
+
 train_dataset = train_dataset.map(prepare_dataset, num_proc=1)
-print("yaboooo")
 test_dataset = test_dataset.map(prepare_dataset, num_proc=1)
 
-import torch
 
-from dataclasses import dataclass
-
-from typing import Any, Dict, List, Union
-print("zimoooo")
 @dataclass
 class DataCollatorSpeechSeq2SeqWithPadding:
     processor: Any
-    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
 
+    def __call__(
+        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
         # split inputs and labels since they have to be of different lengths and need different padding methods
-
         # first treat the audio inputs by simply returning torch tensors
-
-        input_features = [{"input_features": feature["input_features"]} for feature in features]
-
-        batch = self.processor.feature_extractor.pad(input_features, return_tensors="pt")
-
+        input_features = [
+            {"input_features": feature["input_features"]} for feature in features
+        ]
+        batch = self.processor.feature_extractor.pad(
+            input_features, return_tensors="pt"
+        )
         # get the tokenized label sequences
-
         label_features = [{"input_ids": feature["labels"]} for feature in features]
-
         # pad the labels to max length
-
         labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
-
         # replace padding with -100 to ignore loss correctly
-
-        labels = labels_batch["input_ids"].masked_fill(labels_batch.attention_mask.ne(1), -100)
-
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
         # if bos token is appended in previous tokenization step,
-
         # cut bos token here as it’s append later anyways
-
         if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
-
             labels = labels[:, 1:]
         batch["labels"] = labels
         return batch
 
+
 data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
-print("zamzam")
-import evaluate
 metric = evaluate.load("wer")
+
 
 def compute_metrics(pred):
     pred_ids = pred.predictions
@@ -133,20 +109,16 @@ def compute_metrics(pred):
     wer = 100 * metric.compute(predictions=pred_str, references=label_str)
     return {"wer": wer}
 
-from transformers import WhisperForConditionalGeneration
 
 model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-small")
 model.config.forced_decoder_ids = None
 model.config.suppress_tokens = []
 
-
 # Define the Training Arguments
-
-from transformers import Seq2SeqTrainingArguments
-
-training_args = Seq2SeqTrainingArguments(output_dir="./whisper-small-farsi",  # change to a repo name of your choice
+training_args = Seq2SeqTrainingArguments(
+    output_dir="./Whisper-small-Mozilla",
     per_device_train_batch_size=14,
-    gradient_accumulation_steps=4,  # increase by 2x for every 2x decrease in batch size
+    gradient_accumulation_steps=4,
     learning_rate=1e-5,
     warmup_steps=50,
     max_steps=5000,
@@ -158,7 +130,7 @@ training_args = Seq2SeqTrainingArguments(output_dir="./whisper-small-farsi",  # 
     generation_max_length=225,
     save_steps=100,
     eval_steps=100,
-    # logging_steps=25,
+    logging_steps=25,
     report_to=["tensorboard"],
     load_best_model_at_end=True,
     metric_for_best_model="wer",
@@ -166,7 +138,6 @@ training_args = Seq2SeqTrainingArguments(output_dir="./whisper-small-farsi",  # 
     push_to_hub=False,
 )
 
-from transformers import Seq2SeqTrainer
 trainer = Seq2SeqTrainer(
     args=training_args,
     model=model,
@@ -178,4 +149,3 @@ trainer = Seq2SeqTrainer(
 )
 print("Train Started.")
 trainer.train()
-
