5b6477fc7053dd05959aedabe6a47da425e48e40
Mohammad Amin Khataei <makhataei@gmail.com>
2024-03-13 07:15:14 +0000

    Dev

--- src/train.py
+++ src/train.py
@@ -1,8 +1,14 @@
 # import cudf.pandas
 # cudf.pandas.install()
-from datasets import Dataset
-import pandas as pd
+from dataclasses import dataclass
+from typing import Any, Dict, List, Union
+from config import settings
+import evaluate
 import librosa
+import pandas as pd
+import torch
+from datasets import Dataset
+from huggingface_hub import login
 from transformers import (
     WhisperFeatureExtractor,
     WhisperTokenizer,
@@ -11,25 +17,22 @@ from transformers import (
     Seq2SeqTrainer,
     WhisperForConditionalGeneration,
 )
-import torch
-from dataclasses import dataclass
-from typing import Any, Dict, List, Union
-import evaluate
-
-from huggingface_hub import interpreter_login, login
 
 # interpreter_login()
 login(
     token="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",
-add_to_git_credential=True,
-new_session=True,
-write_permission=True
+    add_to_git_credential=True,
+    new_session=True,
+    write_permission=True,
 )
 ## we will load the both of the data here.
-train_df = pd.read_csv("../datasets/Common_Voice_Corpus_15/fa/train1.csv")
-test_df = pd.read_csv("../datasets/Common_Voice_Corpus_15/fa/test.csv")
-PATH = "/home/makhataei/Projects/STT/datasets/Common_Voice_Corpus_15/fa/clips/"
-
+train_df = pd.read_csv(
+    "../datasets/Common_Voice_Corpus_16/fa/train.csv", on_bad_lines="skip"
+)
+test_df = pd.read_csv(
+    "../datasets/Common_Voice_Corpus_16/fa/test.csv", on_bad_lines="skip"
+)
+PATH = "../datasets/Common_Voice_Corpus_16/fa/clips/"
 audio1 = []
 for i in list(train_df.path):
     audio1.append(librosa.load(path=PATH + i, sr=16000)[0])
@@ -88,7 +91,7 @@ class DataCollatorSpeechSeq2SeqWithPadding:
     processor: Any
 
     def __call__(
-        self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
+            self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
     ) -> Dict[str, torch.Tensor]:
         # split inputs and labels since they have to be of different lengths and need different padding methods
         # first treat the audio inputs by simply returning torch tensors
@@ -136,9 +139,9 @@ model.config.suppress_tokens = []
 
 # Define the Training Arguments
 training_args = Seq2SeqTrainingArguments(
-    f"Whisper-Small-Common-Voice",
-    # output_dir="/media/makhataei/Backups/Whisper-Small-Common-Voice",
-    per_device_train_batch_size=14,
+    # f"Whisper-Small-Common-Voice",
+    output_dir=f"../models/{settings.MODEL}",
+    per_device_train_batch_size=10,
     gradient_accumulation_steps=4,
     learning_rate=1e-6,
     warmup_steps=50,
@@ -146,7 +149,7 @@ training_args = Seq2SeqTrainingArguments(
     gradient_checkpointing=True,
     # fp16=True,
     evaluation_strategy="steps",
-    per_device_eval_batch_size=14,
+    per_device_eval_batch_size=10,
     predict_with_generate=True,
     generation_max_length=225,
     save_steps=100,
@@ -173,16 +176,16 @@ print("Train Phase Started.")
 trainer.train()
 trainer.create_model_card(
     language="fa",
-    tags ="fa-asr",
+    tags="fa-asr",
     model_name="Whisper Small Persian",
     finetuned_from="openai/whisper-small",
     tasks="transcribe",
-    dataset_tags="mozilla-foundation/common_voice_15_0",
+    dataset_tags="mozilla-foundation/common_voice_16_0",
     dataset="Common Voice 15.0",
     dataset_args="config: fa, split: train,test",
 )
 kwargs = {
-    "dataset_tags": "mozilla-foundation/common_voice_15_0",
+    "dataset_tags": "mozilla-foundation/common_voice_16_0",
     "dataset": "Common Voice 15.0",
     "dataset_args": "config: fa, split: train,test",
     "language": "fa",
