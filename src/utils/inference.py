e1afce91f1f5b37f50ea9ea40d165cbf1d30dcee
makhataei <makhataei@gmail.com>
2024-05-05 10:35:28 +0330

    Model Chooser Prediction

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -39,3 +39,14 @@ def prediction121(image_bytes):
     emotion = class_names[category]
 
     return emotion
+def predicto(model_type,image_bytes):
+    tensor = get_tensor(image_bytes)
+    if model_type == '121':
+        outputs = model121(tensor)
+    elif model_type == '201':
+        outputs = model201(tensor)
+    _, prediction = outputs.max(1)
+    category = prediction.item()
+    emotion = class_names[category]
+
+    return emotion
