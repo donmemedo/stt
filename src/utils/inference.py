410647b9c2eadad6fc316339fdd7f975e4fa37d4
makhataei <makhataei@gmail.com>
2024-05-05 09:51:01 +0330

    SER Inference Updated from 8 to 6.

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -1,9 +1,9 @@
-from src.utils.commons import get_model, get_tensor
+from src.utils.commons import get_model, get_tensor, get_models
 
 class_names = [
     "angry",
-    "calm",
-    "disgust",
+    # "calm",
+    # "disgust",
     "fearful",
     "happy",
     "neutral",
@@ -11,11 +11,29 @@ class_names = [
     "surprised",
 ]
 model = get_model()
+model121 = get_models('/home/makhataei/Projects/CallCanterQC/models/SER_densenet123.pt',121)
+model201 = get_models('/home/makhataei/Projects/CallCanterQC/models/SER_densenet203.pt',201)
 
 
 def prediction(image_bytes):
     tensor = get_tensor(image_bytes)
     outputs = model(tensor)
+    _, prediction = outputs.max(1)
+    category = prediction.item()
+    emotion = class_names[category]
+
+    return emotion
+def prediction201(image_bytes):
+    tensor = get_tensor(image_bytes)
+    outputs = model201(tensor)
+    _, prediction = outputs.max(1)
+    category = prediction.item()
+    emotion = class_names[category]
+
+    return emotion
+def prediction121(image_bytes):
+    tensor = get_tensor(image_bytes)
+    outputs = model121(tensor)
     _, prediction = outputs.max(1)
     category = prediction.item()
     emotion = class_names[category]
